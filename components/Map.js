df2a7471a1fb09aee1c995754967b4e0ea5ccfc7
RuslanD <drumm2k@gmail.com>
2020-05-01 12:44:06 +0300

    Tried canvas Map cirles as markers Decided to keed usual ones for now

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup, LayerGroup } from 'react-leaflet';
 import { LeafletStyles, MarkerClusterStyles } from './MapStyles';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
@@ -8,12 +8,10 @@ let coords = [];
 
 // Generate random coordinate
 function generateRandomCoord(min, max) {
-  let randomCoord = Math.random() * (min - max) + max;
-
-  return randomCoord;
+  return Math.random() * (min - max) + max;
 }
 
-// Generate random coordiantes
+// Generate random coordinates func
 function generateCoords(id) {
   // x, y min-max values
   const amin = 59.5;
@@ -29,8 +27,8 @@ function generateCoords(id) {
   });
 }
 
-// Working slow with so many DOM elements, need to use canvas or webgl
-for (let x = 0; x < 9900; x++) {
+// Generate X random coordinates
+for (let x = 0; x < 500; x++) {
   generateCoords(x);
 }
 
@@ -41,21 +39,50 @@ class MyMap extends Component {
   };
 
   render() {
+    // Canvas Markers shaped as circles (good performance)
+    // Need to import CircleMarker from react-leaflet
+
+    // const MarkersOnCanvas = coords.map((marker) => (
+    //   <CircleMarker
+    //     key={`marker_${marker.id}`}
+    //     center={[marker.x, marker.y]}
+    //     color={'#8b32a8'}
+    //     weight={7}
+    //     radius={3}
+    //   >
+    //     <Popup>
+    //       <div>User {marker.id}</div>
+    //       <div>Trainer Code: 123456789</div>
+    //     </Popup>
+    //   </CircleMarker>
+    // ));
+
+    // Usual Markers decided to keep this for now (medium performance)
+    const Markers = coords.map((marker) => (
+      <Marker key={marker.id} position={[marker.x, marker.y]}>
+        <Popup>
+          <div>User {marker.id}</div>
+          <div>Trainer Code: 123456789</div>
+        </Popup>
+      </Marker>
+    ));
+
     return (
       <div>
         <LeafletStyles />
         <MarkerClusterStyles />
-        <Map center={this.state.center} zoom={this.state.zoom}>
+        <Map
+          preferCanvas={true}
+          center={this.state.center}
+          zoom={this.state.zoom}
+        >
           <TileLayer
             url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+            attribution=""
           />
+          {/* {MarkersOnCanvas} */}
           <MarkerClusterGroup>
-            {coords.map((marker) => (
-              <Marker key={marker.id} position={[marker.x, marker.y]}>
-                <Popup>{marker.id}</Popup>
-              </Marker>
-            ))}
+            <LayerGroup>{Markers}</LayerGroup>
           </MarkerClusterGroup>
         </Map>
       </div>
