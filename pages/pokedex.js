bab1f706ae9083548ec5718de1796257168d0e2f
RuslanD <drumm2k@gmail.com>
2020-05-08 03:37:58 +0300

    Pokedex & tasks pages

---
+++ pages/pokedex.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  color: #ff3163;
+  margin-bottom: 1.5rem;
+`;
+
+const Pokedex = () => (
+  <div>
+    <Title>Покедекс</Title>
+  </div>
+);
+
+export default Pokedex;
---
+++ pages/tasks.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  color: #ff3163;
+  margin-bottom: 1.5rem;
+`;
+
+const Tasks = () => (
+  <div>
+    <Title>Задания</Title>
+  </div>
+);
+
+export default Tasks;
