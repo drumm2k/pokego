b6a3510e6662b1a8999baf976f08a5c8f22eb30c
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-06-08 11:13:03 +0300

    Pokedex -> simple search via name or id

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -4,22 +4,39 @@ import PropTypes from 'prop-types';
 import PokedexList from './PokedexList';
 
 const FilterInput = styled.input`
-  width: 100%;
-  margin-right: 1rem;
-  margin-bottom: 1rem;
+  width: 20rem;
+  margin: 1rem;
 `;
 
 export default class Pokedex extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { searchTerm: '', pokemons: this.props.pokemons };
   }
 
+  onChangeValue = (event) => {
+    const result = this.props.pokemons.filter(
+      (pokemon) =>
+        pokemon.pokedex.pokemonNum == event.target.value ||
+        pokemon.pokemonId.includes(event.target.value.toUpperCase())
+    );
+    this.setState({ searchTerm: event.target.value, pokemons: result });
+  };
+
   render() {
-    const { pokemons } = this.props;
+    const { pokemons } = this.state;
     return (
       <>
-        <FilterInput />
+        <label>
+          Search:
+          <FilterInput
+            type="text"
+            name="search"
+            value={this.state.searchTerm}
+            onChange={this.onChangeValue}
+          />
+        </label>
+
         <PokedexList pokemons={pokemons} />
       </>
     );
