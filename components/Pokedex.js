3fd1f9fd38bf38426a2f5172c51f4f15fe2cc080
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-07-03 07:15:14 +0300

    Pokedex -> moved Tabs, Search & Gen labels

--- components/Pokedex.js
+++ components/Pokedex.js
@@ -10,6 +10,7 @@ const FilterTabs = styled.ul`
   text-align: center;
   flex-wrap: wrap;
   color: #000;
+  margin-bottom: 1rem;
 `;
 
 const FilterTabsItem = styled.li`
@@ -32,13 +33,19 @@ const FilterTabsItem = styled.li`
 const FiltersContainer = styled.div`
   margin: 1rem 0;
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const FilterLabel = styled.label`
+  margin: 0.5rem 0;
 `;
 
 const FilterSearchInput = styled.input`
-  width: 100%;
+  width: 25rem;
   border: 1px solid rgb(220, 220, 220);
   padding: 0.5rem 1rem;
+  margin-left: 1rem;
 
   &:focus {
     background-color: transparent;
@@ -51,9 +58,9 @@ const FilterSearchInput = styled.input`
 const StyledSelect = styled.select`
   border: 1px solid rgb(220, 220, 220);
   width: 20rem;
-  margin: 0.5rem 0;
   /* appearance: none; */
   padding: 0.5rem 1rem;
+  margin-left: 1rem;
 
   &:focus {
     border: 1px solid rgb(150, 150, 150);
@@ -150,6 +157,39 @@ export default class Pokedex extends Component {
 
     return (
       <>
+        <FiltersContainer>
+          <FilterLabel>
+            Поиск:
+            <FilterSearchInput
+              type="text"
+              name="search"
+              placeholder="Имя или номер покемона"
+              value={searchTerm}
+              onChange={this.filterSearch}
+            />
+          </FilterLabel>
+          <FilterLabel>
+            Поколения:
+            <StyledSelect value={gen} onChange={this.filterGen}>
+              <option defaultValue value="All">
+                Все
+              </option>
+              <option value="GEN_1">1 - Kanto</option>
+              <option value="GEN_2">2 - Johto</option>
+              <option value="GEN_3">3 - Hoenn</option>
+              <option value="GEN_4">4 - Sinnoh</option>
+              <option value="GEN_5">5 - Unova</option>
+              <option value="GEN_7">7 - Alola</option>
+              <option value="GEN_8">8 - Galar</option>
+            </StyledSelect>
+          </FilterLabel>
+
+          <Checkbox
+            label="Только легендарные и мифические"
+            checked={legendaryOnly}
+            onChange={this.filterLegendary}
+          />
+        </FiltersContainer>
         <FilterTabs>
           <FilterTabsItem
             onClick={this.filterTabs}
@@ -173,42 +213,6 @@ export default class Pokedex extends Component {
             Шайни
           </FilterTabsItem>
         </FilterTabs>
-        <FiltersContainer>
-          <FilterSearchInput
-            type="text"
-            name="search"
-            placeholder="Введите имя или номер покемона"
-            value={searchTerm}
-            onChange={this.filterSearch}
-          />
-
-          <StyledSelect
-            value={gen}
-            onChange={this.filterGen}
-            aria-expanded="false"
-            aria-autocomplete="list"
-            aria-haspopup="listbox"
-            role="combobox"
-          >
-            <option defaultValue value="All">
-              Все поколения
-            </option>
-            <option value="GEN_1">1 - Kanto</option>
-            <option value="GEN_2">2 - Johto</option>
-            <option value="GEN_3">3 - Hoenn</option>
-            <option value="GEN_4">4 - Sinnoh</option>
-            <option value="GEN_5">5 - Unova</option>
-            <option value="GEN_7">7 - Aloha</option>
-            <option value="GEN_8">8 - Galar</option>
-          </StyledSelect>
-
-          <Checkbox
-            label="Только легендарные и мифические"
-            checked={legendaryOnly}
-            onChange={this.filterLegendary}
-          />
-        </FiltersContainer>
-
         <PokedexList
           pokemons={filteredPokemons}
           showModal={this.showModal}
