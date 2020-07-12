248cdf96ca1764feff7a4fabdd28b3e1a2a421b6
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-07-12 04:21:06 +0300

    Profile -> Add copy trainer code functionality

--- components/ProfileTrainerCode.js
+++ components/ProfileTrainerCode.js
@@ -1,30 +1,43 @@
+import { useState, useRef } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import ClipboardCoryIcon from '../assets/clipboard_copy.svg';
+
+import ClipboardCopyIcon from '../assets/clipboard_copy.svg';
 
 const TrainerCodeContainer = styled.div`
   display: flex;
   align-items: center;
 `;
 
-const IconContainer = styled.button`
-  width: 2.4rem;
-  height: 2.4rem;
+const CopyButton = styled.button`
+  width: 2.3rem;
+  height: 2.3rem;
   margin-left: 0.5rem;
 `;
 
-const ProfileTrainerCode = ({ trainerCode }) => {
+function ProfileTrainerCode({ trainerCode }) {
   if (!trainerCode) return null;
+
+  function copyTrainerCode() {
+    const textField = document.createElement('textarea');
+    textField.innerText = trainerCode;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  }
 
   return (
     <TrainerCodeContainer>
-      <p>Код тренера: {trainerCode}</p>
-      <IconContainer>
-        <ClipboardCoryIcon stroke="rgb(90, 90, 90)" />
-      </IconContainer>
+      <p>
+        Код тренера: <span>{trainerCode}</span>
+      </p>
+      <CopyButton onClick={copyTrainerCode}>
+        <ClipboardCopyIcon stroke="rgb(90, 90, 90)" />
+      </CopyButton>
     </TrainerCodeContainer>
   );
-};
+}
 
 ProfileTrainerCode.propTypes = {
   trainerCode: PropTypes.string,
