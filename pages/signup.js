862cec57a5fe16fbd17c0bf3ff4c4e05b9b25824
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-07-24 14:19:55 +0300

    Add signup email subscription field

--- pages/signup.js
+++ pages/signup.js
@@ -3,7 +3,7 @@ import { gql, useMutation } from '@apollo/client';
 import styled from 'styled-components';
 
 import Title from '../components/Title';
-import { Button, Input, Label, Radio } from '../components/UI';
+import { Button, Input, Label, Radio, Checkbox } from '../components/UI';
 
 const Form = styled.form`
   display: grid;
@@ -14,6 +14,11 @@ const Form = styled.form`
   border-radius: ${(p) => p.theme.border.radius300};
   box-shadow: ${(p) => p.theme.lighting.shadow300};
   padding: ${(p) => p.theme.spacing.s16} ${(p) => p.theme.spacing.s20};
+
+  p {
+    font-size: ${(p) => p.theme.font.size.sm};
+    color: ${(p) => p.theme.color.warning};
+  }
 `;
 
 const FormField = styled.section`
@@ -27,6 +32,7 @@ export const SIGNUP = gql`
     $userName: String!
     $email: String!
     $password: String!
+    $subscription: Boolean!
     $team: String!
     $level: Int!
     $code: String!
@@ -36,6 +42,7 @@ export const SIGNUP = gql`
         userName: $userName
         email: $email
         password: $password
+        subscription: $subscription
         trainer: { team: $team, level: $level, code: $code }
       }
     ) {
@@ -51,13 +58,16 @@ function SignUp() {
   const [team, setTeam] = useState('');
   const [level, setLevel] = useState('');
   const [code, setCode] = useState('');
+  const [subscription, setSubscription] = useState(true);
 
-  function completed() {
-    console.log('completed');
-  }
+  const [message, setMessage] = useState('');
 
-  const [signUp, { data, loading, error: apiError }] = useMutation(SIGNUP, {
-    onCompleted: completed,
+  const [signUp, { loading, error }] = useMutation(SIGNUP, {
+    onCompleted() {
+      setMessage(
+        `Письмо для подтверждения аккаунта отправлено на ваш адрес: ${email}`
+      );
+    },
   });
 
   const handleInputChange = (event) => {
@@ -82,6 +92,9 @@ function SignUp() {
       case 'code':
         setCode(value);
         break;
+      case 'subscription':
+        setSubscription(!subscription);
+        break;
       default:
         break;
     }
@@ -101,6 +114,7 @@ function SignUp() {
         userName,
         email,
         password,
+        subscription,
         team,
         level,
         code,
@@ -108,119 +122,145 @@ function SignUp() {
     });
   }
 
+  const renderErrors = (err) => {
+    let errorMessage;
+
+    if (err) {
+      errorMessage = err.message;
+    }
+
+    if (errorMessage) {
+      return <p>{errorMessage}</p>;
+    }
+    return null;
+  };
+
   return (
     <>
       <Title color="#666">Регистрация</Title>
-      {apiError && <div>{JSON.stringify(apiError)}</div>}
-      {data && <div>{JSON.stringify(data.signUp)}</div>}
-
-      <Form onSubmit={signUpHandler}>
-        <FormField>
-          <Label htmlFor="userName" bold>
-            Имя (как в игре)
-          </Label>
-          <Input
-            type="text"
-            id="userName"
-            name="userName"
-            onChange={handleInputChange}
-            value={userName}
-          />
-        </FormField>
-        <FormField>
-          <Label htmlFor="email" bold>
-            Почта
-          </Label>
-          <Input
-            type="email"
-            id="email"
-            name="email"
-            autocomplete="email"
-            onChange={handleInputChange}
-            value={email}
-          />
-        </FormField>
-        <FormField>
-          <Label htmlFor="password" bold>
-            Пароль
-          </Label>
-          <Input
-            type="password"
-            id="password"
-            name="password"
-            autocomplete="new-password"
-            onChange={handleInputChange}
-            value={password}
-          />
-        </FormField>
-
-        <FormField>
-          <Label htmlFor="team" bold>
-            Команда
-          </Label>
-          <Label>
-            <Radio
-              id="teamValor"
-              name="team"
-              value="valor"
+      {!message && (
+        <Form onSubmit={signUpHandler}>
+          <FormField>
+            <Label htmlFor="userName" bold>
+              Имя (как в игре)
+            </Label>
+            <Input
+              type="text"
+              id="userName"
+              name="userName"
               onChange={handleInputChange}
+              value={userName}
             />
-            Valor
-          </Label>
-          <Label>
-            <Radio
-              id="teamMystic"
-              name="team"
-              value="mystic"
+          </FormField>
+          <FormField>
+            <Label htmlFor="email" bold>
+              Почта
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              autocomplete="email"
               onChange={handleInputChange}
+              value={email}
             />
-            Mystic
-          </Label>
-          <Label>
-            <Radio
-              id="teamInstinct"
-              name="team"
-              value="instinct"
+          </FormField>
+          <FormField>
+            <Label htmlFor="password" bold>
+              Пароль
+            </Label>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              autocomplete="new-password"
               onChange={handleInputChange}
+              value={password}
             />
-            Instinct
-          </Label>
-        </FormField>
-        <FormField>
-          <Label htmlFor="level" bold>
-            Уровень
-          </Label>
-          <Input
-            type="number"
-            inputmode="numeric"
-            pattern="\d*"
-            id="level"
-            name="level"
-            min="1"
-            max="40"
-            onChange={handleInputChange}
-            value={level}
-          />
-        </FormField>
-        <FormField>
-          <Label htmlFor="code" bold>
-            Код тренера
-          </Label>
-          <Input
-            type="text"
-            pattern="[\d*]{12}"
-            placeholder="xxxx xxxx xxxx"
-            id="code"
-            name="code"
-            onChange={handleInputChange}
-            value={code}
-          />
-        </FormField>
+          </FormField>
 
-        <Button bg="accent" color="white">
-          {loading ? <>Загрузка</> : <>Зарегистрироваться</>}
-        </Button>
-      </Form>
+          <FormField>
+            <Label htmlFor="team" bold>
+              Команда
+            </Label>
+            <Label>
+              <Radio
+                id="teamValor"
+                name="team"
+                value="valor"
+                onChange={handleInputChange}
+              />
+              Valor
+            </Label>
+            <Label>
+              <Radio
+                id="teamMystic"
+                name="team"
+                value="mystic"
+                onChange={handleInputChange}
+              />
+              Mystic
+            </Label>
+            <Label>
+              <Radio
+                id="teamInstinct"
+                name="team"
+                value="instinct"
+                onChange={handleInputChange}
+              />
+              Instinct
+            </Label>
+          </FormField>
+          <FormField>
+            <Label htmlFor="level" bold>
+              Уровень
+            </Label>
+            <Input
+              type="number"
+              inputmode="numeric"
+              pattern="\d*"
+              id="level"
+              name="level"
+              min="1"
+              max="40"
+              onChange={handleInputChange}
+              value={level}
+            />
+          </FormField>
+          <FormField>
+            <Label htmlFor="code" bold>
+              Код тренера
+            </Label>
+            <Input
+              type="text"
+              pattern="[\d*]{12}"
+              placeholder="xxxx xxxx xxxx"
+              id="code"
+              name="code"
+              onChange={handleInputChange}
+              value={code}
+            />
+          </FormField>
+          <FormField>
+            <Label>
+              <Checkbox
+                checked={subscription}
+                id="subscription"
+                name="subscription"
+                onChange={handleInputChange}
+              />
+              Подписаться на новости
+            </Label>
+          </FormField>
+
+          {renderErrors(error)}
+
+          <Button bg="accent" color="white">
+            {loading ? <>Загрузка</> : <>Зарегистрироваться</>}
+          </Button>
+        </Form>
+      )}
+      {message && <div>{message}</div>}
     </>
   );
 }
