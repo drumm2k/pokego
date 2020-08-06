d267fe38b97592ac264015a57ca868dd05f3c82f
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-08-06 22:52:10 +0300

    Add raids refresh button for admin

--- pages/raids.js
+++ pages/raids.js
@@ -1,6 +1,9 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { useContext } from 'react';
 import RaidTier from '../components/Raids/RaidTier';
 import Title from '../components/Title';
+import { Button } from '../components/UI';
+import AuthContext from '../context/auth';
 import { initializeApollo } from '../lib/apolloClient';
 
 export const GET_ALL_RAIDS = gql`
@@ -26,8 +29,22 @@ export const GET_ALL_RAIDS = gql`
   }
 `;
 
+export const REFRESH_RAIDS = gql`
+  mutation {
+    initRaids {
+      id
+    }
+  }
+`;
+
 export default function Raids() {
+  const auth = useContext(AuthContext);
   const { data, loading, error } = useQuery(GET_ALL_RAIDS);
+  const [refresh, { error: refreshError }] = useMutation(REFRESH_RAIDS, {
+    onCompleted() {
+      console.log('Refreshed successfully');
+    },
+  });
 
   if (error) return <div>Error</div>;
   if (loading) return <div>Loading</div>;
@@ -40,6 +57,10 @@ export default function Raids() {
       {getRaids.map((tier) => (
         <RaidTier key={tier.id} id={tier.tier} tiersData={tier} />
       ))}
+      {auth.user && auth.user.roles.includes('admin') && (
+        <Button onClick={refresh}>Refresh Raids</Button>
+      )}
+      {refreshError && <p>{refreshError.message}</p>}
     </>
   );
 }
